6073de70f2669cb927b70df59bbc5504783264dd
drechslerderek <drechsler.derek@yahoo.com>
2023-09-13 02:33:11 -0600

    ui form controls

--- assets/controllers/global-dom-controller.js
+++ assets/controllers/global-dom-controller.js
@@ -3,19 +3,44 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
 
     connect() {
+        const audio = new Audio('/build/sounds/menu-selection-102220.mp3');
+        const rollover = this.debounce(() => {
+            let resp = audio.play();
+
+            if (resp!== undefined) {
+                resp.then(_ => {
+                    // autoplay starts!
+                }).catch(error => {
+                    //show error
+                });
+            }
+        },150);
+
         document.querySelectorAll('button, a').forEach((element) => {
             element.addEventListener('mouseover',() => {
-                let audio = new Audio('/build/sounds/menu-selection-102220.mp3');
-                let resp = audio.play();
-
-                if (resp!== undefined) {
-                    resp.then(_ => {
-                        // autoplay starts!
-                    }).catch(error => {
-                        //show error
-                    });
-                }
+                rollover()
             })
         })
+
+        let navLinks = document.querySelectorAll('.nav-link')
+        navLinks.forEach((element) => {
+            element.addEventListener('click', (event) => {
+                navLinks.forEach((navLink) => {
+                    navLink.classList.remove('active')
+                })
+                event.target.classList.add('active')
+            })
+        })
+    }
+
+    debounce(cb, delay) {
+        let timeout
+
+        return (...args) => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                cb(...args)
+            }, delay)
+        }
     }
 }
--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -16,10 +16,38 @@ class ViewController extends AbstractController
 //        return $this->render('',[]);
 //    }
 
+    #[Route('/main', name: 'mainView')]
+    #[Cache(maxage: 3600, public: true)]
+    public function mainView(): Response
+    {
+        return $this->render('views/main.html.twig',['view' => 'shipyard']);
+    }
+
     #[Route('/shipyard', name: 'shipyardView')]
     #[Cache(maxage: 3600, public: true)]
     public function shipyardView(): Response
     {
         return $this->render('views/shipyard.html.twig',['view' => 'shipyard']);
+    }
+
+    #[Route('/view-parts', name: 'partsView')]
+    #[Cache(maxage: 3600, public: true)]
+    public function partsView(): Response
+    {
+        return $this->render('views/view-parts.html.twig',['view' => 'view-parts']);
+    }
+
+    #[Route('/create-part', name: 'createPartView')]
+    #[Cache(maxage: 3600, public: true)]
+    public function createPartView(): Response
+    {
+        return $this->render('views/create-part.html.twig',['view' => 'create-part']);
+    }
+
+    #[Route('/build-ship', name: 'buildShipView')]
+    #[Cache(maxage: 3600, public: true)]
+    public function buildShipView(): Response
+    {
+        return $this->render('views/build-ship.html.twig',['view' => 'build-ship']);
     }
 }
