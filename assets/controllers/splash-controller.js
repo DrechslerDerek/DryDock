f2670ce9d2c4e29af3e93cf0eacca06777505b6c
drechslerderek <drechsler.derek@yahoo.com>
2023-09-14 13:48:57 -0600

    ui form controls

--- assets/controllers/splash-controller.js
+++ assets/controllers/splash-controller.js
@@ -1,19 +1,35 @@
 import { Controller } from '@hotwired/stimulus';
 
+/**
+ * @property {HTMLElement} splashDivTarget
+ */
 export default class extends Controller {
 
+    static targets = [
+        'splashDiv'
+    ]
     connect() {
-        // const audio = new Audio('/build/sounds/Mass-Effect-Trilogy-Extended-Gal.mp3');
-        // // audio.loop = true;
-        // let resp = audio.play();
-        //
-        // if (resp !== undefined) {
-        //     resp.then(_ => {
-        //
-        //         // autoplay starts!
-        //     }).catch(error => {console.log(error)
-        //         //show error
-        //     });
-        // }
+
+    }
+
+    showSplash(event){
+        event.target.classList.add('d-none')
+        this.splashDivTarget.classList.remove('d-none')
+        this.play()
+    }
+
+    play(){
+        const audio = new Audio('/build/sounds/Mass-Effect-Trilogy-Extended-Gal.mp3');
+        // audio.loop = true;
+        let resp = audio.play();
+
+        if (resp !== undefined) {
+            resp.then(_ => {
+
+                // autoplay starts!
+            }).catch(error => {console.log(error)
+                //show error
+            });
+        }
     }
 }
